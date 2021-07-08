139afee40bd6002cb452c768ae219d0aa854f08e
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2021-07-09 08:29:45 +0900

    add conv_str2timeval(char *, struct timeval *)

--- set_timer.h
+++ set_timer.h
@@ -2,6 +2,8 @@
 #define _SET_TIMER 1
 
 #include <sys/time.h>
+#include <ctype.h>
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -10,6 +12,7 @@
 extern int set_timer(long sec, long usec, long sec_interval, long usec_interval);
 extern struct timeval float2timeval(double x);
 extern struct timeval str2timeval(char *str);
+extern int conv_str2timeval(char *str, struct timeval *result);
 extern useconds_t str2useconds(char *str);
 extern int wait_alarm();
 #endif
