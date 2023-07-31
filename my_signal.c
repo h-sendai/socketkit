b0d6c644f7018cc8e0e055a11411b40825e8f127
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2023-07-31 11:10:06 +0900

    Add comment on why special case for SIGALRM

--- my_signal.c
+++ my_signal.c
@@ -8,6 +8,11 @@ Sigfunc *my_signal(int signo, Sigfunc *func)
     sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
     if (signo == SIGALRM) {
+/* The reason for making a special case of SIGALRM is that the purpose
+   of generating this signal is normally to place a timeout on an I/O
+   operation, as we will show in Section 14.2, in which case, we want
+   the blocked system call to be interrupted by the signal
+   (Stevens, Fenner, Rudoff "UNIX Network Programming Section 5.8 (p. 131) */
 #ifdef  SA_INTERRUPT
         act.sa_flags |= SA_INTERRUPT;   /* SunOS 4.x */
 #endif
